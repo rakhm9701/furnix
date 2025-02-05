3d473f98ea91c62a31ecea80a3b6b78b905470da
David <odilovrakhmidin.gmail.com>
2025-02-06 03:14:15 +0500

    feat: integration of comment logics

--- apps/furnix-api/src/components/components.module.ts
+++ apps/furnix-api/src/components/components.module.ts
@@ -3,6 +3,8 @@ import { MemberModule } from './member/member.module';
 import { LikeService } from './like/like.service';
 import { LikeModule } from './like/like.module';
 import { ViewResolver } from './view/view.resolver';
+import { CommentModule } from './comment/comment.module';
+import { CommentResolver } from './comment/comment.resolver';
 import { FollowModule } from './follow/follow.module';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
@@ -11,7 +13,7 @@ import { ViewModule } from './view/view.module';
 import { ViewResolver } from './view/view.resolver';
 
 @Module({
-  imports: [MemberModule, LikeModule, ViewModule, AuthModule, ProductModule, FollowModule],
-  providers: [LikeService, ViewResolver, AuthService]
+  imports: [MemberModule, LikeModule, ViewModule, AuthModule, ProductModule, FollowModule, CommentModule],
+  providers: [LikeService, ViewResolver, AuthService, CommentResolver]
 })
 export class ComponentsModule {}
