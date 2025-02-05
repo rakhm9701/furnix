edd684a08a3fabff22861c202a8465053a44a457
David <odilovrakhmidin.gmail.com>
2025-02-05 23:43:31 +0500

    feat: creating components  module

--- apps/furnix-api/src/app.module.ts
+++ apps/furnix-api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, ComponentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
