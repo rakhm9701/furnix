b715da558bcd96a4a756001a35ea7b41142e9525
David <odilovrakhmidin.gmail.com>
2025-02-06 04:08:45 +0500

    feat: createing database module on furnix-batch server

--- apps/furnix-batch/src/furnix-batch.module.ts
+++ apps/furnix-batch/src/furnix-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FurnixBatchController } from './furnix-batch.controller';
 import { FurnixBatchService } from './furnix-batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [FurnixBatchController],
   providers: [FurnixBatchService],
 })
