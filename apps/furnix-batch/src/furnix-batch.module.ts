8497f0d93ea0d1e1fb383fa0e26d8d6dd20a06cb
David <odilovrakhmidin.gmail.com>
2025-02-05 22:18:08 +0500

    feat: configure env vatiables

--- apps/furnix-batch/src/furnix-batch.module.ts
+++ apps/furnix-batch/src/furnix-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FurnixBatchController } from './furnix-batch.controller';
 import { FurnixBatchService } from './furnix-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [FurnixBatchController],
   providers: [FurnixBatchService],
 })
--- apps/furnix-batch/src/main.ts
+++ apps/furnix-batch/src/main.ts
@@ -3,6 +3,6 @@ import { FurnixBatchModule } from './furnix-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(FurnixBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
